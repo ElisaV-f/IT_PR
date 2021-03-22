392f7500f8ce1780c98499076b664f0ec9991b85
Лиза Зыбайло <zybilo8935511@gmail.com>
2021-03-22 11:54:33 +0300

    2021

---
+++ Pr-5/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace PR_5
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.button1 = new System.Windows.Forms.Button();
+      this.listBox1 = new System.Windows.Forms.ListBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.label3 = new System.Windows.Forms.Label();
+      this.index_string = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(579, 374);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 1;
+      this.button1.Text = "Пуск";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // listBox1
+      // 
+      this.listBox1.FormattingEnabled = true;
+      this.listBox1.Items.AddRange(new object[] {
+            "Практическая работа 4",
+            "Задача организации, в особенности же рамки и место",
+            "обучения кадров в значительной степени",
+            "обуславливает создание форм воздействия."});
+      this.listBox1.Location = new System.Drawing.Point(85, 77);
+      this.listBox1.Name = "listBox1";
+      this.listBox1.Size = new System.Drawing.Size(332, 251);
+      this.listBox1.TabIndex = 2;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(441, 379);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(43, 13);
+      this.label1.TabIndex = 3;
+      this.label1.Text = "1. Итог";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Location = new System.Drawing.Point(444, 412);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(43, 13);
+      this.label2.TabIndex = 4;
+      this.label2.Text = "2. Итог";
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(604, 34);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(100, 20);
+      this.textBox1.TabIndex = 6;
+      this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Location = new System.Drawing.Point(484, 37);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(102, 13);
+      this.label3.TabIndex = 7;
+      this.label3.Text = "Введите фрагмент";
+      // 
+      // index_string
+      // 
+      this.index_string.Location = new System.Drawing.Point(579, 412);
+      this.index_string.Name = "index_string";
+      this.index_string.Size = new System.Drawing.Size(75, 23);
+      this.index_string.TabIndex = 1;
+      this.index_string.Text = "Пуск";
+      this.index_string.UseVisualStyleBackColor = true;
+      this.index_string.Click += new System.EventHandler(this.index_string_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.textBox1);
+      this.Controls.Add(this.label2);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.listBox1);
+      this.Controls.Add(this.index_string);
+      this.Controls.Add(this.button1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Button index_string;
+  }
+}
+
---
+++ Pr-5/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR_5
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+    //1.	Подсчитать, сколько раз среди символов заданной строки встречается буква «а».
+    private void button1_Click(object sender, EventArgs e)
+        {
+      
+        // Получаем номер выделенной строки
+        int index = listBox1.SelectedIndex;
+        // Считываем строку в перменную str
+        string str = (string)listBox1.Items[index];
+        // Узнаем количество символов в строке
+        int len = str.Length;
+        // Считаем, что количество пробелов равно 0
+        int count = 0;
+        // Устанавливаем счетчик символов в 0
+        int i = 0;
+        //Организуем цикл перебора всех символов в строке
+        while (i < len - 1)
+        {
+        // Если нашли пробел, то увеличиваем
+        // счетчик пробелов на 1
+        if (str[i] == 'а')
+          count++;
+        i++;
+      }
+      label1.Text = "Символов А в строке = " + count.ToString();
+    }
+
+    //14.	Подсчитать, сколько раз встречается в тексте заданный фрагмент.
+
+
+    
+
+
+    private void index_string_Click(object sender, EventArgs e)
+    {
+
+      {
+        // Получаем номер выделенной строки
+        int index = listBox1.SelectedIndex;
+        // Считываем строку в перменную str
+        string str = (string)listBox1.Items[index];
+        // Узнаем количество символов в строке
+        int len = str.Length;
+        // Считаем, что количество пробелов равно 0
+        int count = 0;
+        // Устанавливаем счетчик символов в 0
+        int i = 0;
+        //Организуем цикл перебора всех символов в строке
+
+        string str_textbox = textBox1.Text;
+        int index_string = 0;
+        if (listBox1.SelectedIndex != null)
+        {
+          index_string = Convert.ToString(listBox1.SelectedItem).Length;
+        }
+        else MessageBox.Show("Select string, sir");
+        Console.WriteLine(index_string);
+
+     
+
+        
+          char[] string_to_char = new char[index_string];
+
+          for (int index_for_char = 0; index_for_char <= index_string; index_for_char++)
+          {
+          string_to_char = str.ToCharArray();
+          
+          }
+        //        char[] string_vveli = new char[];
+
+        
+
+
+
+       
+        label2.Text = "Заданный фрагмент встречается = " + count.ToString() + "раз";
+      }
+     
+  
+
+    }
+
+    private void textBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
